ebde3af9efbef4a5d1b372bc114a1901052afe90
Romain <dudu.romain.8@gmail.com>
2025-02-10 16:06:20 +0100

    Bonus filter finished

--- front/src/app/products/features/product-list/product-list.component.ts
+++ front/src/app/products/features/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, inject, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { Product } from "app/products/data-access/product.model";
 import { ProductsService } from "app/products/data-access/products.service";
 import { ProductFormComponent } from "app/products/ui/product-form/product-form.component";
@@ -7,6 +8,8 @@ import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { DataViewModule } from 'primeng/dataview';
 import { DialogModule } from 'primeng/dialog';
+import { InputNumberModule } from "primeng/inputnumber";
+import { InputTextModule } from "primeng/inputtext";
 
 const emptyProduct: Product = {
   id: 0,
@@ -30,7 +33,7 @@ const emptyProduct: Product = {
   templateUrl: "./product-list.component.html",
   styleUrls: ["./product-list.component.scss"],
   standalone: true,
-  imports: [DataViewModule, CardModule, ButtonModule, DialogModule, ProductFormComponent],
+  imports: [DataViewModule, CardModule, ButtonModule, DialogModule, ProductFormComponent, FormsModule, InputTextModule, InputNumberModule],
 })
 export class ProductListComponent implements OnInit {
   private readonly productsService = inject(ProductsService);
@@ -41,10 +44,17 @@ export class ProductListComponent implements OnInit {
   public isCreation = false;
   public readonly editedProduct = signal<Product>(emptyProduct);
 
+  filteredProducts= [...this.products()];
+  searchTerm: string = '';
+  minPrice: number | null = null;
+  maxPrice: number | null = null;
+
   constructor(private cartService: CartService) {}
 
   ngOnInit() {
-    this.productsService.get().subscribe();
+    this.productsService.get().subscribe(() => {
+      this.filterProducts();
+    });
   }
 
   public onCreate() {
@@ -83,4 +93,18 @@ export class ProductListComponent implements OnInit {
   public addToCart(product: Product) {
     this.cartService.addToCart(product);
   }
+
+  filterProducts() {
+    const min = this.minPrice ?? 0;
+    const max = this.maxPrice ?? Infinity;
+  
+    this.filteredProducts = this.products().filter((product: Product) => {
+      const matchesName = product.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+  
+      const matchesMinPrice = product.price >= min;
+      const matchesMaxPrice = product.price <= max;
+  
+      return matchesName && matchesMinPrice && matchesMaxPrice;
+    });
+  }  
 }
